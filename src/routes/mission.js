9f1d4bee9831262a9fec23aea1d4e56924e30589
YoungSeokLee <ylee102@gmail.com>
2024-11-15 21:10:24 +0900

    mission상태 변경하기

--- src/routes/mission.js
+++ src/routes/mission.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   handleMemberMissionCreate,
   handleMemberMissionListReadByStatus,
+  handleMissionCompletion
 } from "../controllers/mission.controller.js";
 
 const router = express.Router();
@@ -9,4 +10,5 @@ const router = express.Router();
 router.post("/:missionId", handleMemberMissionCreate);
 router.get("/mine/:memberId", handleMemberMissionListReadByStatus);
 
+router.patch("/:missionId", handleMissionCompletion)
 export default router;
