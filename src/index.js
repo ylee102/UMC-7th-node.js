d49da297a379dd4896af90a6e76515cd52a1abc8
YoungSeokLee <ylee102@gmail.com>
2024-11-19 14:07:49 +0900

    회원가입 swagger 주석추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 import member from "./routes/member.js";
 import mission from "./routes/mission.js";
@@ -11,14 +15,55 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+
 app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
+//swagger
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = [
+    "./src/index.js",
+    "./routes/member.js",  // 회원 관련 API 라우트
+    "./routes/store.js",   // 스토어 관련 API 라우트
+    "./routes/mission.js" // 미션관련 API 라우트. 
+  ];
+  const doc = {
+    info: {
+      title: "UMC 7th",
+      description: "UMC 7th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
+
 
 app.use("/members", member);
 app.use("/stores", store);
