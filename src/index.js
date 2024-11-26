64aa6792682908ecd19c80f80be84872edf65a9a
YoungSeokLee <ylee102@gmail.com>
2024-11-26 15:17:53 +0900

    googlt Oauth login

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import dotenv from "dotenv";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import { prisma } from './db.config.js';
+
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import session from "express-session";
+import passport from "passport";
+import { googleStrategy } from "./auth.config.js";
+
+
 import swaggerAutogen from "swagger-autogen";
 import swaggerUiExpress from "swagger-ui-express";
 
@@ -10,7 +18,12 @@ import member from "./routes/member.js";
 import mission from "./routes/mission.js";
 import store from "./routes/store.js";
 
+
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const app = express();
 const port = process.env.PORT;
@@ -40,6 +53,30 @@ app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  // #swagger.ignore = true
+  res.send("Hello World!");
+});
 
 //swagger
 app.use(
@@ -83,11 +120,20 @@ app.get("/", (req, res) => {
 });
 
 
-
+app.get("/oauth2/login/google", passport.authenticate("google"));
+app.get(
+  "/oauth2/callback/google",
+  passport.authenticate("google", {
+    failureRedirect: "/oauth2/login/google",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
+);
 
 app.use("/members", member);
 app.use("/stores", store);
 app.use("/missions", mission);
+
 
 /**
  * 전역 오류를 처리하기 위한 미들웨어
